cf1d9208574170280f227923539c27d7b8d036dc
unknown <ma.santanat@alumnos.duoc.cl>
2020-12-02 16:52:12 -0300

    API externa

--- TiendaHarkov/TiendaHarkov/settings.py
+++ TiendaHarkov/TiendaHarkov/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Juegos.apps.JuegosConfig',
     'sesion.apps.SesionConfig',
+    'rest_framework',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10 
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
